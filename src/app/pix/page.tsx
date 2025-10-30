f5dc7d741d4be628a3aa8471b8bb2b3b252c8986
Mateus Queirós <mateuscqueiros@gmail.com>
2025-10-30 11:39:58 -0300

    pix page

---
+++ src/app/pix/page.tsx
@@ -0,0 +1,17 @@
+'use client';
+
+import { CreatePixForm } from '@/features/transactions/forms/create-pix-form';
+import { toast } from 'sonner';
+
+export default function PixPage() {
+  const handleSubmit = (values: any) => {
+    console.log(values);
+    toast.success('Pagamento efetuado com sucesso');
+  };
+
+  return (
+    <div className="h-full flex flex-col bg-base-100">
+      <CreatePixForm onSubmitAction={handleSubmit} />
+    </div>
+  );
+}
